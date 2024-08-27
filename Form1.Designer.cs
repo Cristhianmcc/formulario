4405cf961d1409591f87d51d619f53634e6c2177
Cristhianmcc <cristhianm@iestplurin.edu.pe>
2024-08-27 10:45:38 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_conectar = new System.Windows.Forms.Button();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.datagrid = new System.Windows.Forms.DataGridView();
+            this.lbl_mensaje = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.datagrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_conectar
+            // 
+            this.btn_conectar.Location = new System.Drawing.Point(38, 418);
+            this.btn_conectar.Name = "btn_conectar";
+            this.btn_conectar.Size = new System.Drawing.Size(121, 38);
+            this.btn_conectar.TabIndex = 0;
+            this.btn_conectar.Text = "CONECTAR";
+            this.btn_conectar.UseVisualStyleBackColor = true;
+            this.btn_conectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.BackColor = System.Drawing.Color.DarkGray;
+            this.btn_agregar.Location = new System.Drawing.Point(12, 327);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(116, 39);
+            this.btn_agregar.TabIndex = 1;
+            this.btn_agregar.Text = "AGREGAR";
+            this.btn_agregar.UseVisualStyleBackColor = false;
+            this.btn_agregar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(182, 327);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "LIMPIAR";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(358, 327);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "SALIR";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "USERNAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PASSWORD";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "EMAIL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MENSAJE";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(261, 49);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(153, 26);
+            this.txt_username.TabIndex = 9;
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(261, 151);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(153, 26);
+            this.txt_email.TabIndex = 12;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(261, 105);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(153, 26);
+            this.txt_pass.TabIndex = 13;
+            // 
+            // datagrid
+            // 
+            this.datagrid.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.datagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagrid.Location = new System.Drawing.Point(520, 33);
+            this.datagrid.Name = "datagrid";
+            this.datagrid.RowHeadersWidth = 62;
+            this.datagrid.RowTemplate.Height = 28;
+            this.datagrid.Size = new System.Drawing.Size(687, 351);
+            this.datagrid.TabIndex = 14;
+            this.datagrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lbl_mensaje
+            // 
+            this.lbl_mensaje.AutoSize = true;
+            this.lbl_mensaje.Location = new System.Drawing.Point(257, 197);
+            this.lbl_mensaje.Name = "lbl_mensaje";
+            this.lbl_mensaje.Size = new System.Drawing.Size(85, 20);
+            this.lbl_mensaje.TabIndex = 15;
+            this.lbl_mensaje.Text = "MENSAJE";
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(293, 418);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(121, 38);
+            this.btn_calcular.TabIndex = 16;
+            this.btn_calcular.Text = "CALCULAR";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 577);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.lbl_mensaje);
+            this.Controls.Add(this.datagrid);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.btn_conectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.datagrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_conectar;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.DataGridView datagrid;
+        private System.Windows.Forms.Label lbl_mensaje;
+        private System.Windows.Forms.Button btn_calcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formulario
+{
+    public partial class Form1 : Form
+
+    {
+        MySqlConnection connection;
+
+        private int id_selected = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            String connectionString = "Server=localhost;" +
+                "Database= usuario;" +
+                "User ID = root;" +
+                " Password = ;" +
+                " SslMode = ; ";
+            connection = new MySqlConnection(connectionString);
+            ListarTodo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+           
+
+
+        }
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ListarTodo()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("ID");
+            dt.Columns.Add("USUARIO");
+            dt.Columns.Add("CONTRASEÑA");
+            dt.Columns.Add("CORREO");
+            
+
+
+            string query = "SELECT * FROM users";
+            MySqlCommand cmd = new MySqlCommand(query, this.connection);
+
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32("id");
+                    string username = reader.GetString("username");
+                    string password = reader.GetString("password");
+                    string email = reader.GetString("email");
+
+                    dt.Rows.Add(id, username, password, email);
+
+                    
+
+                }
+            }
+            catch (Exception ex)
+            {
+                lbl_mensaje.Text += "Error en Lista: " + ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            datagrid.DataSource = dt;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            crearUsuario();
+            ListarTodo();
+        }
+
+        private void crearUsuario()
+        {
+            string query = "INSERT INTO users (username,password,email)" +
+                " VALUES (@username,@password,@email)";
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(query, this.connection);
+                cmd.Parameters.AddWithValue("@username", txt_username.Text);
+                cmd.Parameters.AddWithValue("@password", txt_pass.Text);
+                cmd.Parameters.AddWithValue("@email", txt_email.Text);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                lbl_mensaje.Text += "Registro insertado correctamente";
+            }
+            catch (Exception ex) {
+                lbl_mensaje.Text += "Error en registro: " + ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            crearUsuario();
+            ListarTodo();
+        }
+    }
+}
+        
+    
+
